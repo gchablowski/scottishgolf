0045762414c36a3f90e4146502ab16515a641fb6
gerald Chablowski <g.chablowski@gmail.com>
2017-03-01 11:15:38 +0100

    add false data

--- app/main/services/main-serv.js
+++ app/main/services/main-serv.js
@@ -1,22 +1,86 @@
 'use strict';
 angular.module('main')
-.service('Main', function ($log, $timeout) {
+        .service('MainServ', function () {
 
-  $log.log('Hello from your Service: Main in module main');
+            this.getGolfData = function () {
 
-  // some initial data
-  this.someData = {
-    binding: 'Yes! Got that databinding working'
-  };
+                var datas =
+                        [
+                            {
+                                id: 1,
+                                name: 'Golspie Golf Club',
+                                address: 'Ferry Road, Golspie, Sutherland, KW10 6ST',
+                                region: 66,
+                                phone: '01408 633266',
+                                img: 'main/assets/images/golf1.jpg',
+                                membership: true,
+                                membershipCard: false,
+                                clubGolf: false,
+                                getIntoGolf: false
+                            },
+                            {
+                                id: 2,
+                                name: 'Longniddry Golf Club',
+                                address: 'Links road, Longniddry, EH32 0NL',
+                                region: 65,
+                                phone: '01408 633266',
+                                img: 'main/assets/images/golf2.jpg',
+                                membership: false,
+                                membershipCard: true,
+                                clubGolf: false,
+                                getIntoGolf: false
+                            },
+                            {
+                                id: 3,
+                                name: 'Tain Golf Club',
+                                address: 'Chapel Road, Tain, Ross-shire, IV19 1JE',
+                                region: 67,
+                                phone: '01862 892314',
+                                img: 'main/assets/images/golf3.jpg',
+                                membership: false,
+                                membershipCard: false,
+                                clubGolf: true,
+                                getIntoGolf: false
+                            },
+                            {
+                                id: 4,
+                                name: 'Golspie Golf Club2',
+                                address: 'Ferry Road, Golspie, Sutherland, KW10 6ST',
+                                region: 68,
+                                phone: '01408 633266',
+                                img: 'main/assets/images/golf1.jpg',
+                                membership: false,
+                                membershipCard: false,
+                                clubGolf: false,
+                                getIntoGolf: true
+                            },
+                            {
+                                id: 5,
+                                name: 'Tain Golf Club2',
+                                address: 'Chapel Road, Tain, Ross-shire, IV19 1JE',
+                                region: 69,
+                                phone: '01862 892314',
+                                img: 'main/assets/images/golf3.jpg',
+                                membership: true,
+                                membershipCard: true,
+                                clubGolf: false,
+                                getIntoGolf: false
+                            },
+                            {
+                                id: 6,
+                                name: 'Longniddry Golf Club2',
+                                address: 'Links road, Longniddry, EH32 0NL',
+                                region: 65,
+                                phone: '01408 633266',
+                                img: 'main/assets/images/golf2.jpg',
+                                membership: false,
+                                membershipCard: false,
+                                clubGolf: true,
+                                getIntoGolf: true
+                            }
+                        ]
+                        ;
 
-  this.changeBriefly = function () {
-    var initialValue = this.someData.binding;
-    this.someData.binding = 'Yeah this was changed';
-
-    var that = this;
-    $timeout(function () {
-      that.someData.binding = initialValue;
-    }, 500);
-  };
-
-});
+                return datas;
+            };
+        });
